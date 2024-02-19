0332d3c6c43557eeb7f4b6d3d64f7642c3a950c5
sonyerg <ericksonrguinto@gmail.com>
2024-02-19 17:03:01 +1300

    added player functionality

---
+++ src/components/Player.jsx
@@ -0,0 +1,37 @@
+import React, { useState } from "react";
+
+export default function Player({ initialName, symbol }) {
+  const [isEditing, setIsEditing] = useState(false);
+  const [playerName, setPlayerName] = useState(initialName);
+
+  function handleEditClick() {
+    // setIsEditing(isEditing ? false : true);
+    // if your new state depends on your previous state value, you should not update the state like this:
+    // setIsEditing(!isEditing); // => schedules a state update to true
+    // Instead, pass a function to your state updating function:
+    setIsEditing((editing) => !editing); // Best practice!
+  }
+
+  function handleNameChange(event) {
+    setPlayerName(event.target.value);
+  }
+
+  return (
+    <li>
+      <span className="player">
+        {isEditing ? (
+          <input
+            type="text"
+            value={playerName}
+            onChange={handleNameChange}
+            required
+          />
+        ) : (
+          <span className="player-name">{playerName}</span>
+        )}
+        <span className="symbol">{symbol}</span>
+      </span>
+      <button onClick={handleEditClick}>{isEditing ? "Save" : "Edit"}</button>
+    </li>
+  );
+}
